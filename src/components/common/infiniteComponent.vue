559133bce3ee978bb82da1af5aff2d4d70c214d2
Jo-ormi <sunjooos2@naver.com>
2023-08-24 11:31:57 +0900

    feat: 메인 페이지 레이아웃 및 상품 목록 조회[#3 #13]

---
+++ src/components/common/infiniteComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <ul ref="listEl">
+        <li v-for="comment in commentsList">
+          {{ comment }}
+        </li>
+      </ul>
+    </div>
+</template>
+
+<script setup>
+
+import { axios } from "axios";
+
+ const getComments = async(max, omit) => {
+  try {
+    const comments = await axios.get(
+      `https://jsonplaceholder.typicode.com/comments?_limit=${max}&_start=${omit}`
+    );
+    return comments.data.map((comment) => comment.body);
+  } catch (error) {
+    console.error(error);
+  }
+}
+</script>
