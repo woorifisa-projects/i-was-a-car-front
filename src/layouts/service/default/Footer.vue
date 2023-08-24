9d23d154ee7a5cf1b71857a1c280313cebc3e046
Jo-ormi <sunjooos2@naver.com>
2023-08-24 11:35:31 +0900

    feat: footer 글 작성 [#13]

--- src/layouts/service/default/Footer.vue
+++ src/layouts/service/default/Footer.vue
@@ -13,11 +13,12 @@
           :href="icon.to" target="_blank">
           {{ icon.name }}</v-btn
         >
-      </div>
+      </div><br/>
       <div class="pt-0">
-        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet.
-        Mauris cursus commodo interdum. Praesent ut risus eget metus luctus
-      </div>
+        대표 이사: 황동민 | 주소: 서울특별시 상암 어딘가<br/><br/>
+        I was a car 가 제공하는 서비스는 온라인 거래장소 및 기타 부가정보 제공에 한하며, 실제 거래의 모든 책임은 판매자와 구매자에게 있음을 안내드립니다.<br/>
+        본 사이트/앱 상의 모든 정보, 콘텐츠, UI 등에 대한 무단 복제, 배포, 스크래핑 등의 행위는 법에 의하여 엄격히 금지됩니다.
+      </div><br/>
 
       <v-divider></v-divider>
 
