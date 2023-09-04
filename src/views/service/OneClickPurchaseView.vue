86d0d081c53586806c319bb30dc1021cb75b3315
Jo-ormi <sunjooos2@naver.com>
2023-09-04 09:22:47 +0900

    일단 저장

--- src/views/service/OneClickPurchaseView.vue
+++ src/views/service/OneClickPurchaseView.vue
@@ -1,7 +1,7 @@
 <template>
   <BasicInfoView v-if="route.params.id === '1'"></BasicInfoView>
-  <CarTypeView v-else-if="route.params.id === '2'"></CarTypeView>
-  <SearchCarView v-else-if="route.params.id === '3'"></SearchCarView>
+  <!-- <CarTypeView v-else-if="route.params.id === '2'"></CarTypeView>
+  <SearchCarView v-else-if="route.params.id === '3'"></SearchCarView> -->
   <ChoiceCarView v-else-if="route.params.id === '4'"></ChoiceCarView>
   <LoansView v-else-if="route.params.id === '5'"></LoansView>
   <InsurancesView v-else-if="route.params.id === '6'"></InsurancesView>
@@ -13,8 +13,8 @@
 import { useRoute } from 'vue-router';
 
 import BasicInfoView from '@/views/service/purchase/BasicInfoView.vue';
-import CarTypeView from '@/views/service/purchase/CarTypeView.vue';
-import SearchCarView from '@/views/service/purchase/SearchCarView.vue';
+// import CarTypeView from '@/views/service/purchase/CarTypeView.vue';
+// import SearchCarView from '@/views/service/purchase/SearchCarView.vue';
 import ChoiceCarView from '@/views/service/purchase/ChoiceCarView.vue';
 import LoansView from '@/views/service/purchase/LoansView.vue';
 import InsurancesView from '@/views/service/purchase/InsurancesView.vue';
