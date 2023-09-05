f5eaff4cfc7b44627ac85c859f92029a8e22778a
lc i m , m i n s u <68263815+MSlcim@users.noreply.github.com>
2023-09-05 17:45:00 +0900

    [feat] my page tab 기능 구현 (#28)
    * feat : 마이페이지 구매내역, 판매내역, 멤버 정보 페이지 구현
    
    * feat: 임시저장
    
    * feat: 대작업 전 수정한거 백업 with 선주
    
    * feat: tab기능 구현 [#4]
    
    * refactor: 콘솔 로그 지움[#4]
    
    * refactor: flag 함수 수정[#4]

---
+++ src/views/service/mypage/MemberInfoView.vue
@@ -0,0 +1,193 @@
+<template>
+  <div>
+    <div class="mx-auto mt-10" style="max-width: 800px; font-size: 1.1em">
+      <v-table density="compact">
+        <thead>
+          <tr>
+            <th style="width: 70%; font-weight: 800; color: black">회원</th>
+            <th style="width: 30%; font-weight: 800; color: black">내용</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>이름</th>
+
+            <th style="text-align: left">
+              <input
+                v-model="orderData.name"
+                ref="firstInput"
+                :readonly="!editable"
+              />
+            </th>
+          </tr>
+
+          <tr>
+            <th>이메일</th>
+            <th style="text-align: left">
+              <input v-model="orderData.email" :readonly="true" />
+            </th>
+          </tr>
+
+          <tr>
+            <th>패스워드</th>
+            <th style="text-align: left"><input :readonly="!editable" /></th>
+          </tr>
+
+          <tr>
+            <th>패스워드확인</th>
+            <th style="text-align: left"><input :readonly="!editable" /></th>
+          </tr>
+
+          <tr>
+            <th>전화번호</th>
+            <th style="text-align: left">
+              <input v-model="orderData.tel" :readonly="!editable" />
+            </th>
+          </tr>
+
+          <tr>
+            <th>면허 유무</th>
+            <th style="text-align: left">
+              <span v-if="!editable">{{
+                orderData.hasLicense === 'true' ? '있음' : '없음'
+              }}</span>
+
+              <div v-else>
+                <input
+                  type="radio"
+                  id="license-yes"
+                  value="true"
+                  v-model="orderData.hasLicense"
+                />
+                <label for="license-yes">있음</label>
+                <input
+                  type="radio"
+                  id="license-no"
+                  value="false"
+                  v-model="orderData.hasLicense"
+                />
+                <label for="license-no">없음</label>
+              </div>
+            </th>
+          </tr>
+
+          <tr>
+            <th>성별</th>
+            <th style="text-align: left">
+              <span v-if="!editable">{{
+                orderData.gender === '남자' ? '남자' : '여자'
+              }}</span>
+
+              <div v-else>
+                <input
+                  type="radio"
+                  id="man"
+                  value="남자"
+                  v-model="orderData.gender"
+                />
+                <label for="man">남자</label>
+                <input
+                  type="radio"
+                  id="woman"
+                  value="여자"
+                  v-model="orderData.gender"
+                />
+                <label for="license-no">여자</label>
+              </div>
+            </th>
+          </tr>
+
+          <tr>
+            <th>최근 접속일</th>
+            <th style="text-align: left">
+              <input v-model="orderData.lastLoginAt" :readonly="true" />
+            </th>
+          </tr>
+
+          <tr>
+            <th>회원가입일</th>
+            <th style="text-align: left">
+              <input v-model="orderData.createdAt" :readonly="true" />
+            </th>
+          </tr>
+        </tbody>
+      </v-table>
+    </div>
+
+    <div style="display: flex; justify-content: center">
+      <v-btn :width="120" size="x-large" class="font-weight-black my-2">
+        취소
+      </v-btn>
+
+      <div style="width: 30px"></div>
+      <v-btn
+        :width="120"
+        size="x-large"
+        class="bg-black font-weight-black my-2"
+        @click="toggleEditing"
+      >
+        {{ editable ? '저장하기' : '수정하기' }}
+      </v-btn>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onBeforeMount, onMounted, nextTick } from 'vue';
+import { memberDetailApi } from '@/apis/service/histories/memberInfoApi.js';
+
+const orderData = ref([]);
+
+const firstInput = ref(null);
+
+const editable = ref(false);
+
+const fetchData = async () => {
+  try {
+    const memberId = 1; // 예시로 memberId 설정
+    const res = await memberDetailApi(memberId);
+
+    orderData.value = res.data.data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
+const toggleEditing = () => {
+  if (editable.value) {
+    // 저장 로직 추가...
+
+    // 저장 후 편집 불가능 상태로 전환
+    editable.value = false;
+  } else {
+    // 편집 가능 상태로 전환 후 첫 번째 입력 필드에 포커스
+    editable.value = true;
+
+    nextTick(() => {
+      firstInput.value.focus();
+    });
+  }
+};
+
+onBeforeMount(() => {
+  fetchData();
+});
+</script>
+
+<style lang="scss" scoped>
+.v-table {
+  width: 100%; /* 너비 설정 */
+  height: 500px; /* 높이 설정 */
+}
+.v-table th {
+  text-align: center;
+}
+
+.center {
+  text-align: center;
+}
+
+th v-text-field {
+  width: 100%;
+}
+</style>
---
+++ src/views/service/mypage/PurchaseHistoryDetailView.vue
@@ -0,0 +1,181 @@
+<template>
+  <v-container style="height: 50vh" class="h-screen">
+    <div class="d-flex justify-space-around">
+      <div class="mr-5">
+        <h2 style="text-align: center; margin-bottom: 1.5rem">탁송정보</h2>
+
+        <v-table style="width: 600px" class="mx-auto">
+          <thead>
+            <tr>
+              <th style="width: 55%; font-weight: 800; color: black">정보</th>
+              <th style="width: 45%; font-weight: 800; color: black">내용</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <th>수령인</th>
+
+              <th style="text-align: left">
+                {{ deliveryData.memberName }}
+              </th>
+            </tr>
+
+            <tr>
+              <th>연락처</th>
+              <th style="text-align: left">
+                {{ deliveryData.memberTel }}
+              </th>
+            </tr>
+
+            <tr>
+              <th>우편번호</th>
+              <th style="text-align: left">{{ deliveryData.zipCode }}</th>
+            </tr>
+
+            <tr>
+              <th>탁송 주소</th>
+              <th style="text-align: left">{{ deliveryData.address }}</th>
+            </tr>
+
+            <tr>
+              <th>탁송 예정일</th>
+              <th style="text-align: left">
+                {{ deliveryData.deliverySchedule }}
+              </th>
+            </tr>
+          </tbody>
+        </v-table>
+      </div>
+
+      <v-divider class="ms-3" inset vertical></v-divider>
+
+      <div class="ml-5">
+        <h2 style="text-align: center; margin-bottom: 1.5rem">계약정보</h2>
+        <v-table style="width: 600px" class="mx-auto">
+          <thead>
+            <tr>
+              <th style="width: 55%; font-weight: 800; color: black">정보</th>
+              <th style="width: 45%; font-weight: 800; color: black">내용</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <th>차량명</th>
+
+              <th style="text-align: left">
+                {{ contractData.productName }}
+              </th>
+            </tr>
+
+            <tr>
+              <th>계약 일자</th>
+              <th style="text-align: left">
+                {{ contractData.createdAt }}
+              </th>
+            </tr>
+
+            <tr>
+              <th>차량 가격</th>
+              <th style="text-align: left">{{ contractData.productPrice }}</th>
+            </tr>
+
+            <tr>
+              <th>은행</th>
+              <th style="text-align: left">{{ contractData.bankName }}</th>
+            </tr>
+
+            <tr>
+              <th>예금주</th>
+              <th style="text-align: left">
+                {{ contractData.accountHolder }}
+              </th>
+            </tr>
+
+            <tr>
+              <th>계좌번호</th>
+              <th style="text-align: left">
+                {{ contractData.accountNumber }}
+              </th>
+            </tr>
+
+            <tr>
+              <th>대출 정보</th>
+              <th style="text-align: left">
+                {{ contractData.loanName }}
+              </th>
+            </tr>
+
+            <tr>
+              <th>보험 정보</th>
+              <th style="text-align: left">
+                {{ contractData.insuranceName }}
+              </th>
+            </tr>
+          </tbody>
+        </v-table>
+      </div>
+    </div>
+
+    <div style="text-align: center; margin-top: 2rem">
+      <v-btn
+        :width="120"
+        size="x-large"
+        class="bg-black font-weight-black my-2"
+        @click="$emit('historyList')"
+      >
+        목록으로 가기
+      </v-btn>
+    </div>
+  </v-container>
+</template>
+
+<script setup>
+import { ref, onBeforeMount, defineEmits } from 'vue';
+import { purchaseHistoryDetailAPI } from '@/apis/service/histories/historyApi';
+import { useRoute, useRouter } from 'vue-router';
+
+const emit = defineEmits(['historyList']);
+
+const route = useRoute();
+const router = useRouter();
+
+const purchaseHistoryNo = route.params.id;
+
+const deliveryData = ref({});
+const contractData = ref({});
+
+const fetchData = async () => {
+  try {
+    const memberId = 1; // 예시로 memberId 설정
+    const res = await purchaseHistoryDetailAPI(memberId, purchaseHistoryNo);
+
+    deliveryData.value = {
+      memberName: res.data.data.memberName,
+      memberTel: res.data.data.memberTel,
+      zipCode: res.data.data.zipCode,
+      address: res.data.data.address,
+      deliverySchedule: res.data.data.deliverySchedule,
+    };
+    contractData.value = {
+      productName: res.data.data.productName,
+      createdAt: res.data.data.createdAt,
+      productPrice: res.data.data.productPrice,
+      bankName: res.data.data.bankName,
+      accountHolder: res.data.data.accountHolder,
+      accountNumber: res.data.data.accountNumber,
+      loanName: res.data.data.loanName,
+      insuranceName: res.data.data.insuranceName,
+    };
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
+onBeforeMount(() => {
+  fetchData();
+});
+</script>
+
+<style lang="scss" scoped></style>
