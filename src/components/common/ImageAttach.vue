5a2c66abe7a7cc3353c3e008c256828aa3006ecd
lc i m , m i n s u <68263815+MSlcim@users.noreply.github.com>
2023-09-12 09:03:55 +0900

    refactor: 관리자 페이지 검색바 제거 (#50)

--- src/components/common/ImageAttach.vue
+++ src/components/common/ImageAttach.vue
@@ -49,13 +49,11 @@ defineExpose({ input });
   display: flex;
   flex-direction: column;
   align-items: center;
-  height: 300px;
   gap: 20px;
   .uploadFiles__form {
     display: flex;
     flex-direction: column;
     align-items: center;
-    height: 350px;
     .uploadFiles__form-attach {
       width: 100px;
       font-size: 30px;
@@ -73,6 +71,14 @@ defineExpose({ input });
         height: 0;
         widows: 0;
       }
+      // @media (max-width: 980px) {
+      //   width: 100px;
+      //   font-size: 18px;
+      //   label {
+      //     width: 100px;
+      //     height: 100px;
+      //   }
+      // }
     }
     .uploadFiles__form-thumbnail {
       width: 450px;
@@ -83,16 +89,15 @@ defineExpose({ input });
       overflow-x: scroll;
       > div {
         position: relative;
-        margin-top: 20px;
         .thumb {
-          min-width: 120px;
-          height: 120px;
+          width: 200px;
+          height: 200px;
           border-radius: 10px;
         }
         button {
           position: absolute;
-          min-width: 20px;
-          min-height: 20px;
+          min-width: 30px;
+          min-height: 30px;
           aspect-ratio: 1 / 1;
           padding: 0;
           top: -17px;
@@ -103,8 +108,8 @@ defineExpose({ input });
       @media (max-width: 980px) {
         > div {
           .thumb {
-            // width: 100px;
-            // height: 100px;
+            width: 100px;
+            height: 100px;
           }
         }
       }
