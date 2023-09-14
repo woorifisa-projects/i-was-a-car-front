0e3cbc3c335ebc782d0a656f3fd7a585cbb75453
Cheol-Cheol <dream174822@gmail.com>
2023-09-14 17:30:40 +0900

    refactor: 마이페이지 및 기타 등등

--- src/router/index.js
+++ src/router/index.js
@@ -5,16 +5,22 @@ import adminRouter from './admin';
 import { useAuthStore } from '@/store/auth';
 import { storeToRefs } from 'pinia';
 import { authAPI } from '@/apis/service/auth/authApi';
+import { useRouteStore } from '@/store/route';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [...serviceRouter.options.routes, ...adminRouter.options.routes],
 });
 
-router.beforeEach(async (to) => {
+router.beforeEach(async (to, from) => {
   const auth = useAuthStore();
   const { isLogin } = storeToRefs(auth);
   const { verifiedAuth } = auth;
+
+  const routeStore = useRouteStore();
+  const { setPreviousRoute } = routeStore;
+
+  setPreviousRoute(from.path);
 
   try {
     const { data } = await authAPI();
